5a4b5e90925d5537cb57f0602153331a926a77d4
rhythm-chicmic <rhythm.sharma@chicmic.co.in>
2024-10-09 14:22:47 +0530

    feat: testing dynamic users

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -2,6 +2,8 @@ import { useParams } from 'react-router-dom'
 
 function User() {
     const {userid} = useParams()
+    console.log(userid);
+    
   return (
     <div className='bg-gray-600 text-white text-3xl p-4'>User: {userid}</div>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -30,6 +30,5 @@ const router = createBrowserRouter(
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
-    <App />
   </StrictMode>,
 )
